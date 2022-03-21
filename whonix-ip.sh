d77c878908de7e52119363340fe1abf986294d8e
Marks Junior <46227462+marksjr@users.noreply.github.com>
2022-03-20 22:34:49 -0300

    Create whonix-ip.sh

---
+++ whonix-ip.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#https://github.com/marksjr
+clear
+
+while (true)
+
+echo "Trocando IP a cada 5 minutos"
+
+for ((k = 0; k <= 10 ; k++))
+
+do  
+
+    echo -n "[ "
+
+    for ((i = 0 ; i <= k; i++)); do echo -n "###"; done
+
+    for ((j = i ; j <= 10 ; j++)); do echo -n "   "; done
+
+    v=$((k * 10))
+
+    echo -n " ] "
+
+    echo -n "$v %" $'\r'
+
+    sleep 30
+
+done
+
+do
+
+systemctl --no-pager restart tor@default
+
+clear
+
+done
